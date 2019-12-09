7cda3d4ca0dc089d2739d888f0113db44329ad96
neverland251 <43463633+neverland251@users.noreply.github.com>
2019-12-09 16:23:58 +0900

    update arimax

--- Project/DataMining/ARIMAX_Project/series.R
+++ Project/DataMining/ARIMAX_Project/series.R
@@ -1,5 +1,7 @@
 library("ggplot2")
 library("scales")
+library("forecast")
+library("TSA")
 
 ##PAX : 방한관광객수
 ## Cur : 원 - 엔 환율
@@ -7,7 +9,7 @@ library("scales")
 ## FRI_ab : 국가 우호도 조사에서의 우호도 : 한국을 매우 좋아한다(5점 중 5점)
 
 #데이터 파일을 불러온다.
-series <- read.table(file="PAXyear.csv",header=TRUE, sep=",",stringsAsFactors = FALSE)
+series <- read.table(file="C://users//권종익//Documents//MainProject//MainProject//PAXyear.csv",header=TRUE, sep=",",stringsAsFactors = FALSE)
 print(series)
 
 series_test <- series
@@ -25,7 +27,7 @@ plot(PAX, ylab = "PAX", xlab = "Year")
 #방한관광객수 변수의 자기상관함수 그래프를 그린다.
 acf(PAX)
 ## 확인결과 지수함수적으로 감소하지 않아 이 시계열의 공분산이 일정해야한다는 시계열 분석의 기본 가정을 만족하지 못하는 것으로 의심할 수 있다.
-library(forecast)
+
 ndiffs(x = PAX)
 plot(diff(PAX,1))
 acf(diff(PAX,1))
@@ -39,7 +41,7 @@ PAX_1 <- diff(PAX, differences = 1)
 acf(PAX_1)
 pacf(PAX_1)
 
-# auto arima로 적합해본 결과 MA(1)을 추천하였다. 실제로, ARIMA(1,1,1)등 다양한 방법으로 적합을 시도했을때 AR(1)이 가장 BIC가 우수하였다.
+
 PAX_fit <- auto.arima(PAX_1)
 PAX_fit
 
@@ -56,8 +58,8 @@ PAX_diff_log <- diff(log(PAX),differences = 1)
 fri_ab_diff = diff(fri_ab,difference = 1)
 Cur_diff <- diff(Cur,diffrences=1)
 # CCF 확인을 위해 타겟변수인 "방한 관광객수"의 시계열 구조(ARIMA(1,1,0))과 그때의 계수들을 고정시켜놓고 ARIMA모형을 적합한다.
-b = arima(fri_ab, order=c(1,1,0), fixed=c(a$coef))
-c = arima(Cur, order=c(1,1,0), fixed=c(a$coef))
+b = arima(fri_ab, order=c(0,1,1), fixed=c(a$coef))
+c = arima(Cur, order=c(0,1,1), fixed=c(a$coef))
 
 # CCF 확인을 위해 잔차들만 따로 저장을 해준다.
 ## 방한관광객수의 잔차
@@ -77,62 +79,43 @@ ccf(y,z)
 
 ## CCF 결과에 따라 투입계열 Xt의 1차시 변수를 포함하도록 데이터프레임을 만든다.
 ### 투입계열 모수 1, 산출계열 모수 1, 즉 ARIMAX s = 1, r = 1
-su1 = ts.intersect(PAX_diff_log,Cur_diff,Cur_diffd1=lag(Cur_diff,-1))
-### 투입계열 모수 1, 산출계열 모수 0, 즉 ARIMAX s = 1, r = 0 
-su2 = ts.intersect(PAX_diff_log, pax_diffd1=lag(PAX_diff_log,-1),Cur_diff)
+su1 = ts.intersect(PAX_diff_log,Cur_diff)
 
-summary(su2.fit <- lm(PAX_diff_log+ pax_diffd1~ 0+Cur_diff,data=su2))
-summary(su1.fit <- lm(PAX_diff_log~0+Cur_diffd1,data=su1))
+su1.fit <- arimax(su1[,1],order=c(0,0,1),xtransf=su1[,2],transfer=list(c(1,0)),include.mean = FALSE)
 
-### 투입계열 모수1, 산출계열 모수0의 경우가 BIC기준 더 안정적이므로, 이 모델을 수립한다.
-
-## 임시로 모형을 적합했으므로, 이제 잔차들의 ARMA 구조를 파악하여 잔차를 남김없이 여과해준다.
-su1fit <- lm(PAX_diff_log~0+Cur_diffd1,data=su1)
 
 ## 적합한 모형의 잔차들만 따로 저장해준다.
-su1res <- su1fit$residuals
-su1res <- ts(su1res,start = 1989, end = 2016)
+su1res <- su1.fit$residuals
+su1res <- ts(su1res)
 
 
-## 잔차들의 acf와 pacf 구조를 보건데, 이 잔차들은 ACF는 상관관계가 없고 PACF가 6차시 이후로 절단되었으므로, ARMA(0,6)구조를 잠정 추정해볼 수 있다.
+## 잔차들의 acf와 pacf 구조를 보건데, 이 잔차들은 시계열적인 패턴이 없는 완전한 백색잡음이다. 따라서, ARMA(0,0)을 고려한다.
 plot(su1res)
 acf(su1res)
 pacf(su1res)
 
-## 모형 적합용 데이터셋을 설정한다. 타겟변수인 PAX_diff와 환율의 1차시 뒤 값, 잔차들의 1~6차시 뒤 값들을 하나로 모아준다. 
-fitted = ts.intersect(PAX_diff_log ,Cur_diffd1=lag(Cur_diff,-1),su1resd1=lag(su1res,-1),su1resd2=lag(su1res,-2),su1resd3=lag(su1res,-3),su1resd4=lag(su1res,-4),su1resd5=lag(su1res,-5),su1resd6=lag(su1res,-6))
+transf_frame <- data.frame(Cur_diff,su1res)
 
+su1.fit <- arimax(su1[,1],order=c(0,0,1),xtransf=transf_frame,transfer=list(c(1,0),c(0,0)))
+Box.test(su1.fit$residuals,type = c("Ljung-Box"))
 
-library("TSA")
 ## 환율을 auto.arima로 적합한다.
 model1 <- auto.arima(Cur_diff)
 ## 2017년 값을 예측해준 후, 이를 cur_diff_fore 데이터셋에 담는다.
 cur_diff_fore <- predict(model1, h = 1)
-cur_diff_fore <-append(Cur_diff, cur_diff_fore$pred[1])
-cur_diff_fore <- ts(cur_diff_fore, end = 2017)
+cur_diff_fore <- append(Cur_diff, cur_diff_fore$pred[1])
+cur_diff_fore <- ts(cur_diff_fore,start=1989,end=2017)
 
-## 잔차들을 AR(6)으로 1차시 예측해준 후, su1res_fore에 담아준다.
-su1res_fore <- predict(arima(su1res, order=c(6,0,0),include.mean=FALSE),h=1)
-su1res_fore <- append(su1res, su1res_fore$pred[1],after = length(su1res))
-su1res_fore <- ts(su1res_fore, start = 1989, end = 2017)
+model2 <- auto.arima(su1res)
+su1res_fore <- predict(model2,h = 1)
+su1res_fore <- append(su1res, su1res_fore$pred[1])
+su1res_fore <- ts(su1res_fore,start=1989,end=2017)
 
-## 적합용 데이터셋을 설정한다. 2017년 예측 생산값이 담긴 fore데이터셋에서 환율, 잔차들의 1~6차시 뒤 값들을 하나로 모아준다.
-pred <- ts.intersect(cur_diff_fore, su1resd1=lag(su1res_fore,-1),su1resd2=lag(su1res_fore,-2),su1resd3=lag(su1res_fore,-3),su1resd4=lag(su1res_fore,-4),su1resd5=lag(su1res_fore,-5),su1resd6=lag(su1res_fore,-6),su1res_fore)
+transf_fore <- data.frame(cur_diff_fore,su1res_fore)
+colnames(transf_fore) <- colnames(transf_frame)
 
-## 예측용 데이터셋을 생성한다. 2017년 예측을 위해 앞전 단계에서 1차시 예측 생산한 환율과, 잔차들의 데이터를 담는다.
-fitt <- ts(data.frame(pred[23,1],pred[23,8],pred[23,2],pred[23,3],pred[23,4],pred[23,5],pred[23,6]),names=c("cur_diff_fore","su1resd1","su1resd2","su1resd3","su1resd4","su1resd5","su1resd6"))
 
-## arimax 모형을 적합한다. Y의 차수를 ARIMA(1,0,0)으로, "환율"을 xreg로 회귀분석으로 적합하고, 방금 적합한 AR(6)의 잔차는 
-#xtransf에 투입한 후에 그 차수는 ARMA(6,0)으로 최종적으로 필터ㄹ해준다.(model1)
-model1 <- arimax(fitted[,1],order=c(1,0,0),xreg=pred[1:22,1],xtransf=pred[1:22,8],transfer=list(c(6,0)))
-Box.test(model2$residuals,type = c("Ljung-Box"))
+final <- predict(su1.fit,xreg=transf_fore[29,],n.ahead=1)
 
-## 한편, 잔차를 필터로 처리하지 않고, 잔차까지 단순하게 투입변수로 취급한 적합모형도 테스트해준다(model2)
-model2 <- arimax(fitted[,1],order=c(1,0,0),xreg=pred[1:22,0:7])
-Box.test(model1$residuals,type = c("Ljung-Box"))
-## 단순하게 투입변수로 취급한 모형이 AIC, 로그우도 모두 우수하게 나타났다.
-
-final <- predict(model1,newxreg=fitt,n.ahead=1)
-
-exp(14.647503+final$pred)
+abs((2311447 - exp(14.647503+final$pred[1]))/2311447)*100
 # 실측값 2311447과 0.8% 정확도로 예측하였다.
